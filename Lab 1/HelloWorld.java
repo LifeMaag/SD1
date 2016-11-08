54e7fd17a7eb8802a802078b6698c64ed4d9cabf
SUF1aN7 <sufan960@gmail.com>
2016-11-08 15:40:59 +0500

    MY first Lab of SD

---
+++ Lab 1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+     public static void main(String[] args) {
+
+         System.out.println("Hello World!"); // output the string Hello World!.
+    }
+
+ }
---
+++ Lab 1/Taskone.java
@@ -0,0 +1,11 @@
+public class Taskone{
+	public static void main (String[] args){
+		
+		System.out.println("Name:                    Sufyan Abdur Rehman");
+		System.out.println("UOB#                     15026396  ");
+		System.out.println("E-mail:                  Sufyan2015@namal.edu.pk");
+		System.out.println("Percentage of last Year: 68  ");
+		System.out.println("Level of interest:       5");
+	}
+
+}
